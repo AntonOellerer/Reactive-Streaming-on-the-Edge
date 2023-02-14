4fd391b33a0bb8f5ee14c4d01c44bac92607e2f9
Anton Oellerer <aoellerer@protonmail.com>
2023-02-14 08:17:15 +0100

    WIP

--- push_images.sh
+++ push_images.sh
@@ -1,8 +1,8 @@
-docker tag implementation-monitor 127.0.0.1:5000/monitor
-docker tag implementation-sensor 127.0.0.1:5000/sensor
-docker tag implementation-cloud_server 127.0.0.1:5000/cloud_server
+docker tag implementation-monitor $REGISTRY_IP:5000/monitor
+docker tag implementation-sensor $REGISTRY_IP:5000/sensor
+docker tag implementation-cloud_server $REGISTRY_IP:5000/cloud_server
 
-docker push 127.0.0.1:5000/monitor
-docker push 127.0.0.1:5000/sensor
-docker push 127.0.0.1:5000/cloud_server
+docker push $REGISTRY_IP:5000/monitor
+docker push $REGISTRY_IP:5000/sensor
+docker push $REGISTRY_IP:5000/cloud_server
 
