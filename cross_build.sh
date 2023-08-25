f2ad42a6eb30f69e5dc2f47e86dcf3a27a17b2bc
Anton Oellerer <aoellerer@protonmail.com>
2023-08-25 18:24:30 +0200

    release build
    Signed-off-by: Anton Oellerer <aoellerer@protonmail.com>

--- cross_build.sh
+++ cross_build.sh
@@ -1,12 +1,12 @@
 CROSS_CONTAINER_ENGINE=podman
 
 cd motor_driver
-cross build --target aarch64-unknown-linux-gnu
+cross build --target aarch64-unknown-linux-gnu --release
 
 cd ../motor_monitor_oo
-cross build --target aarch64-unknown-linux-gnu
+cross build --target aarch64-unknown-linux-gnu --release
 
 cd ../motor_monitor_rx
-cross build --target aarch64-unknown-linux-gnu
+cross build --target aarch64-unknown-linux-gnu --release
 
 cd ..
